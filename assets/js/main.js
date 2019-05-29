474deec7728c94f82fcad941afb65bbd422bb1e0
SmileyQuiPleure <merenda.etienne@gmail.com>
2019-05-29 12:17:20 +0200

    Architecture fini, textes manquants.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,18 @@
 
 $(function() {
     $("#toggle_button").click(function() {
-        if ($('#wrapper').attr("class") == "sidebar_inactive") {
-            $('#wrapper').attr("class", "sidebar_active");
-            $('#menu').attr('class', 'active')
-            $('#header').attr('class', 'sidebar_active')
+        if ($('#wrapper').attr('class') == 'sidebar_active'){
+            $('#wrapper').attr('class', 'sidebar_inactive');
+            $('#wrapper').css({'width': '100%', 'margin-left': '0%'});
+            $('#menu').css('left', '-40%');
+            $('#header').css({'left': '2.2%'});
+            
         }
-        else {
-            $('#wrapper').attr("class", "sidebar_inactive")
-            $('#menu').attr('class', 'inactive')
-            $('#header').attr('class', 'sidebar_inactive')
-        }
-    })
-});
+        else{
+            $('#wrapper').attr('class', 'sidebar_active');
+            $('#wrapper').css({'width': '80%', 'margin-left': '20%'});
+            $('#menu').css('left', '0%');
+            $('#header').css({'left': '22.2%'});
 
+        }})
+});
